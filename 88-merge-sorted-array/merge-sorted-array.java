9c0ad3f58647cb14b340cc44bb03f1919a0175f9
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2023-11-29 01:10:50 +0530

    Update merge-sorted-array.java

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int j=0;
-		for(int i=m; i<nums1.length; i++) {
-				if(nums1[i]==0) {
-					nums1[i]=nums2[j];
-					j++;
-				}
-			
+    int j=0;				      // j is assigned to zero as pointer for starting of second array
+	for(int i=m; i<nums1.length; i++) {   // In loop starting of iteration we assign i to m 
+		if(nums1[i]==0) {             // checks in the first array after size m , is there any zeros
+			nums1[i]=nums2[j];    // If we find seroes we assign number of second array to first array with index value of j
+			j++;		      // Increment j
 		}
-		Arrays.sort(nums1);
-		System.out.println(Arrays.toString(nums1));
+	}
+	Arrays.sort(nums1);                   // Sort the first array
+	System.out.println(Arrays.toString(nums1));    
     }
 }
