9a8dbd92d736ff55ba0e796c1dfd2400e7796fe5
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2023-11-29 01:32:09 +0530

    Update reverse-integer.java

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,23 +1,23 @@
 class Solution {
     public int reverse(int x) {
         long num = 0;
-        int a, temp=x;
-        if(x<0){
-            x = -x;
+        int a, temp=x;                                           // Declare variable a and temp, temp variable to store the value of x
+        if(x<0){                                                 // Checks if x is negative value
+            x = -x;                                              // If it is negative value, then we convert to positive value 
         }
-        while(x>0){
-            a = x%10;
-            num = (num*10)+a;
-            x = x/10;
+        while(x>0){                                              // Loop iterates till x<0
+            a = x%10;                                            // Extracts the last digit
+            num = (num*10)+a;                                    // Reverse the integer by multiplying by 10 and adding the remainder to it
+            x = x/10;                                            // Removes the last digit
         }
-         if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){
+         if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){     // Gives a minimum and maximum value
             return 0;
         }
-        if(temp<0){
-            return (int)(-1*num);
+        if(temp<0){                                              // If the temporary value is negative 
+            return (int)(-1*num);                                // Return the reversed by multiplying it with -1 to make it a negative integer
         }
         else{
-            return (int)num;
+            return (int)num;                                     // return the reversed number
         }
        
 
