cea8f92c5552cef0fbb043e8745e7bd6b8e86b92
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2023-11-29 01:12:35 +0530

    Update search-in-rotated-sorted-array.java

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int search(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){       // Checks in array if target get matched to number in array
-                return i;
+            if(nums[i]==target){       // Checks in array if target number found in array
+                return i;              // If found return the index 
             }
         }
-        return -1;
+        return -1;                     // If not found then return -1
     }
 }
