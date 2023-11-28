0d371829d0da93af5294615de6f44ee55cb64878
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2023-11-29 01:18:18 +0530

    Update palindrome-number.java

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int num = 0;
-        int a, temp=x;
+        int a, temp=x;            // Assign value of x to a temporary variable temp.
 
-        while(x>0){
-            a = x%10;
-            num = (num*10)+a;
-            x = x/10;
+        while(x>0){               // Iterate until x>0
+            a = x%10;             // Extract the last digit
+            num = (num*10)+a;     // Reversing the number by multiplying and adding the remainder to it.
+            x = x/10;             // Remove the last digit
         }
-        if(temp==num){
-            return true;
+        if(temp==num){            // Checks if reversed number is same as the original number that is stored in temp 
+            return true;          // If is same returns true
         }
         else{
-            return false;
+            return false;         // Else it return false;
         }
     }
 }
