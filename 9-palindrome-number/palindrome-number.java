08549ad52e42449d88203a8322cd290416009e65
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-07-29 10:35:55 +0530

    Time: 5 ms (71.43%) | Memory: 43.9 MB (40.24%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int num = 0;
-        int a, temp=x;            // Assign value of x to a temporary variable temp.
-
-        while(x>0){               // Iterate until x>0
-            a = x%10;             // Extract the last digit
-            num = (num*10)+a;     // Reversing the number by multiplying and adding the remainder to it.
-            x = x/10;             // Remove the last digit
+        int sum=0, temp=x, a;
+        while(x>0){
+            a = x%10;
+            sum = (sum*10)+a;
+            x=x/10;
         }
-        if(temp==num){            // Checks if reversed number is same as the original number that is stored in temp 
-            return true;          // If is same returns true, as it is a palindrome
+        if(temp==sum){
+            return true;
         }
-        else{
-            return false;         // Else it return false, as it is not a palindrome
-        }
+        return false;
     }
 }
