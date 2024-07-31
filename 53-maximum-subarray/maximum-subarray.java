303a7a004060734179e97d140710e9b9c51f2d9a
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-07-31 09:51:28 +0530

    Time: 1 ms (99.59%) | Memory: 57.4 MB (7.78%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,9 +4,9 @@ class Solution {
         int current_sum = 0;
         for(int i=0; i<nums.length; i++){
             current_sum+=nums[i];
-            if(current_sum>max_sum){
-                max_sum = current_sum;
-            }
+            // if(current_sum>max_sum){
+                max_sum = Math.max(current_sum, max_sum);
+            // }
             if(current_sum<0){
                 current_sum=0;
             }
