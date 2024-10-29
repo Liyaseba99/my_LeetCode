b6264aadfcf24d467892b5d42c7d74490ef61c70
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-10-29 11:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (13.14%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,34 +1,41 @@
 class Solution {
-    public void nextPermutation(int[] a) {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
         int ind = -1;
-        for(int i = a.length-2;i>=0;i--){
-            if(a[i]<a[i+1]){
-                ind = i;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
                 break;
             }
         }
         if(ind==-1){
-            arev(a,0,a.length-1);
-            return;
-        }
-        for(int i = a.length-1;i>=0;i--){
-            if(a[i]>a[ind]){
-                int temp = a[i];
-                a[i]=a[ind];
-                a[ind]=temp;
-                break;
+            reverse(nums,0, n-1);
+        }else{
+            for(int i=n-1; i>ind; i--){
+                if(nums[i]>nums[ind]){
+                    swap(nums, i, ind);
+                    break;
+                }
             }
+            reverse(nums, ind+1, n-1);
         }
-        arev(a,ind+1,a.length-1);
-
+        
+        
     }
-        static void arev(int[] a,int low,int high){
-        while(low<high){
-            int temp = a[low];
-            a[low]=a[high];
-            a[high]=temp;
-            low++;
-            high--;
+
+    void reverse(int nums[], int start, int end){
+        int i=start;
+        int j = end;
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
         }
+    }
+
+    void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
