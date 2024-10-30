c762bcb1018f46361c90ab404260713f3ef778f1
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-10-30 22:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (76.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,13 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int j=0;				      // j is assigned to zero as pointer for starting of second array
-	for(int i=m; i<nums1.length; i++) {   // In loop starting of iteration we assign i to m 
-		if(nums1[i]==0) {             // checks in the first array after size m , is there any zeros
-			nums1[i]=nums2[j];    // If we find seroes we assign number of second array to first array with index value of j
-			j++;		      // Increment j
-		}
-	}
-	Arrays.sort(nums1);                   // Sort the first array
-	System.out.println(Arrays.toString(nums1));    
+        int i =m-1;
+        int j=n-1;
+        int k = n+m-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]= nums1[i];
+                i--; 
+            }else{
+                nums1[k]= nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k]= nums2[j];
+            j--;
+            k--;
+        }
     }
 }
