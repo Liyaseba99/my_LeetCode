4ad4c25feb67c35a61282eb1b915c52f03dbdd20
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-11-06 22:58:15 +0530

    Time: 15 ms (44.74%) | Memory: 42.7 MB (82.24%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.java
+++ 259-3sum-smaller/3sum-smaller.java
@@ -1,21 +1,22 @@
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
-        int count = 0;
         Arrays.sort(nums);
-        int len = nums.length;
-    
-        for(int i=0; i<len-2; i++) {
-            int left = i+1, right = len-1;
-            while(left < right) {
-                if(nums[i] + nums[left] + nums[right] < target) {
-                    count += right-left;
-                    left++;
-                } else {
-                    right--;
+        int count=0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum < target){
+                    count+=(k-j);
+                    j++;
+                }else{
+                    k--;
                 }
+
             }
         }
-        
         return count;
     }
 }
