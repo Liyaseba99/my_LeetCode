855557a66312028843c3b99acadc758de1c8de9f
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-11-18 13:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (80.09%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,30 +1,54 @@
 class Solution {
+    //BFS
+    // public boolean isBipartite(int[][] graph) {
+    //     int v = graph.length;
+    //     int[] color = new int[v];
+    //     for(int i=0; i<v; i++) color[i]=-1;
+    //     for(int i=0; i<v; i++){
+    //         if(color[i]==-1){
+    //             if(bfs(i, v, graph, color)==false) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // public boolean bfs(int start, int v, int[][] graph, int[] color){
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(start);
+    //     color[start]=0;
+    //     while(!q.isEmpty()){
+    //         int node = q.peek();
+    //         q.remove();
+    //         for(int it: graph[node]){
+    //             if(color[it]==-1){
+    //                 color[it]= 1 - color[node];
+    //                 q.add(it);
+    //             }else if(color[it]==color[node]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    //DFS
     public boolean isBipartite(int[][] graph) {
         int v = graph.length;
         int[] color = new int[v];
         for(int i=0; i<v; i++) color[i]=-1;
         for(int i=0; i<v; i++){
             if(color[i]==-1){
-                if(bfs(i, v, graph, color)==false) return false;
+                if(dfs(i, 0, graph, color)==false) return false;
             }
         }
         return true;
     }
-    public boolean bfs(int start, int v, int[][] graph, int[] color){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        color[start]=0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            for(int it: graph[node]){
-                if(color[it]==-1){
-                    color[it]= 1 - color[node];
-                    q.add(it);
-                }else if(color[it]==color[node]){
-                    return false;
-                }
-            }
+    public boolean dfs(int node, int col, int[][] graph, int[] color){
+        color[node]=col;
+        for(int it: graph[node]){
+            if(color[it]==-1){
+                if(dfs(it, 1- col, graph, color)==false) return false;
+            }else if(color[it]==color[node]) return false;
         }
         return true;
     }
