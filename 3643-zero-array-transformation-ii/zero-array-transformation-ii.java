5f6128c4777d74ccc309d23d27b7a49e5f8569c8
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-11-24 20:20:50 +0530

    Time: 29 ms (29.79%) | Memory: 118.8 MB (23.60%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int l = 0, r=queries.length+1, n = nums.length;
+        while(l<r){
+            int m = (r+l)/2;
+            int[] diff = new int[n+1];
+            for(int i=0; i<m; i++){
+                int left = queries[i][0];
+                int right = queries[i][1];
+                int val = queries[i][2];
+                diff[left]+=val;
+                if(right+1 < n){
+                    diff[right+1] -=val;
+                }
+            }
+            
+            for(int i=1; i<n+1; i++){
+                diff[i]+=diff[i-1];
+            }
+          
+            boolean flag = true;
+            for(int i=0; i<n; i++){
+               if(nums[i]>diff[i]) {
+                    flag= false;
+                    break;
+               }
+            }
+            if(flag){
+                r=m;
+            }else{
+                l = m+1;
+            }
+        }
+        return l<=queries.length ? l : -1;
+
+        
+    }
+}
