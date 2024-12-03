9b61abed456a972644ef634947b07e8561842116
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2024-12-03 16:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (71.24%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[2];
-        int last =-1, first =-1;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                if(first==-1){
-                    first = i;
-                }
-                last =i;
+        int first = firstOccur(nums, target);
+        if(first==-1) return new int[]{-1,-1};
+        int last = lastOccur(nums, target);
+        return new int[]{first, last};
+    }
+    public int firstOccur(int[] nums, int target){
+        int n = nums.length;
+        int first =-1;
+        int low =0, high =n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                first = mid;
+                high = mid-1;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }else {
+                high = mid-1;
             }
         }
-        arr[0] = first;
-        arr[1] = last;
-        return arr;
+        return first;
+    }
+
+    public int lastOccur(int[] nums, int target){
+        int n = nums.length;
+        int last =-1;
+        int low =0, high =n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                last = mid;
+                low = mid+1;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }else {
+                high = mid-1;
+            }
+        }
+        return last;
     }
 }
