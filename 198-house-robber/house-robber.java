cc53b8409aab06fe59b51f5a585b839b73550156
Liyaseba99 <47812182+Liyaseba99@users.noreply.github.com>
2025-01-22 10:02:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (85.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,21 @@
 class Solution {
+    // tabulation
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
         for(int i=0; i<n; i++){
             dp[i]=-1;
         }
-        return maxCollect(n-1, nums, dp);
-    }
-    public int maxCollect(int ind, int[] nums, int[] dp){
-        if(ind==0) return nums[0];
-        if(ind<1) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pick = nums[ind] + maxCollect(ind-2, nums, dp);
-        int notPick = 0 + maxCollect(ind-1, nums, dp);
-        dp[ind]= Math.max(pick, notPick);
-        return dp[ind];
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+
+            int pick = nums[i];
+            if(i>1){
+                pick += dp[i-2];
+            } 
+            int notPick = 0 + dp[i-1];
+            dp[i]= Math.max(pick, notPick);
+        }
+        return dp[n-1];
     }
 }
